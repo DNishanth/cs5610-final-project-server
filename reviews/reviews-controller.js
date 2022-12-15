3a420508e3b9e9c9fc70919bf8fd3484c245eef0
Nishanth Duraiarasu <dnishanth99@gmail.com>
2022-12-15 15:32:28 -0500

    Get reviews by username

--- reviews/reviews-controller.js
+++ reviews/reviews-controller.js
@@ -3,6 +3,7 @@ import * as reviewsDao from './reviews-dao.js'
 const ReviewsController = (app) => {
     app.post('/api/reviews', postReview);
     app.get('/api/books/:workID/reviews', getReviewsByWorkID);
+    app.get('/api/reviews/:userID/reviews', getReviewsByUserID);
 }
 
 const postReview = async (req, res) => {
@@ -21,7 +22,14 @@ const postReview = async (req, res) => {
 const getReviewsByWorkID = async (req, res) => {
     const workID = req.params.workID;
     const reviews = await reviewsDao.findReviewsByWorkID(workID);
-    console.log(reviews)
+    // console.log(reviews)
+    res.json(reviews);
+}
+
+const getReviewsByUserID = async (req, res) => {
+    const userID = req.params.userID;
+    const reviews = await reviewsDao.findReviewsByUserID(userID);
+    // console.log(reviews)
     res.json(reviews);
 }
 
--- reviews/reviews-dao.js
+++ reviews/reviews-dao.js
@@ -3,4 +3,10 @@ import reviewsModel from "./reviews-model.js";
 export const postReview = (review) => reviewsModel.create(review)
 
 export const findReviewsByWorkID = async (workID) =>
-    await reviewsModel.find({workID}).exec() // TODO: add reviewer once login is ready
+    await reviewsModel.find(
+        {workID})
+        .populate('reviewer')// Include reviewer's fields
+        .exec()
+
+export const findReviewsByUserID = async (userID) =>
+    await reviewsModel.find({userID}).exec()
